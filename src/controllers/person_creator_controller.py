1b77a85b165aed3e6a81cccc81d7c7979211ec9d
Allan <allan.rosendo@hotmail.com>
2024-07-04 14:44:31 -0300

    adding error directory and adding to controllers

--- src/controllers/person_creator_controller.py
+++ src/controllers/person_creator_controller.py
@@ -1,4 +1,5 @@
 import re
+from src.errors.error_types.http_bad_request import HttpBadRequestError
 from src.models.sqlite.interfaces.people_repository import PeopleRepositoryInterface
 from .interfaces.person_creator_controller import PersonCreatorControllerInterface
 
@@ -23,11 +24,11 @@ class PersonCreatorController(PersonCreatorControllerInterface):
         non_valid_caracters = re.compile(r'[^a-z A-Z]')
 
         if non_valid_caracters.search(first_name) or non_valid_caracters.search(last_name):
-            raise Exception("Invalid name")
+            raise HttpBadRequestError("Invalid name")
 
     def __validate_number(self, number: int) -> None:
         if not isinstance(number, int) and number < 0:
-            raise Exception("Invalid number")
+            raise HttpBadRequestError("Invalid number")
 
     def __insert_person_in_db(self, first_name: str, last_name: str, age: int, pet_id: int) -> None:
         self.__people_repository.insert_person(first_name, last_name, age, pet_id)
