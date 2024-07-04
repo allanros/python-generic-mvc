d633d024bb0849c51ef835ac33cd4c993bba9b32
Allan <allan.rosendo@hotmail.com>
2024-07-04 16:51:09 -0300

    updating errors and adding on routes

--- src/main/routes/person_routes.py
+++ src/main/routes/person_routes.py
@@ -1,24 +1,35 @@
+#pylint: disable=R0801
 from flask import Blueprint, jsonify, request
 from src.views.http_types.http_request import HttpRequest
 
 from src.main.composer.person_creator_composer import person_creator_composer
 from src.main.composer.person_finder_composer import person_finder_composer
 
+from src.errors.error_handler import handle_errors
+
 person_route_bp = Blueprint('person_routes', __name__)
 
 @person_route_bp.route('/people', methods=['POST'])
 def list_pets():
-    http_request = HttpRequest(body=request.json)
-    view = person_creator_composer()
+    try:
+        http_request = HttpRequest(body=request.json)
+        view = person_creator_composer()
 
-    response = view.handle(http_request)
+        response = view.handle(http_request)
+        return jsonify(response.body), response.status_code
+    except Exception as exception:
+        response = handle_errors(exception)
+        return jsonify(response.body), response.status_code
 
-    return jsonify(response.body), response.status_code
 
 @person_route_bp.route('/people/<int:person_id>', methods=['GET'])
 def find_person(person_id):
-    http_request = HttpRequest(params={'person_id': person_id})
-    view = person_finder_composer()
+    try:
+        http_request = HttpRequest(params={'person_id': person_id})
+        view = person_finder_composer()
 
-    response = view.handle(http_request)
-    return jsonify(response.body), response.status_code
+        response = view.handle(http_request)
+        return jsonify(response.body), response.status_code
+    except Exception as exception:
+        response = handle_errors(exception)
+        return jsonify(response.body), response.status_code
--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -4,21 +4,30 @@ from src.views.http_types.http_request import HttpRequest
 from src.main.composer.pet_lister_composer import pet_lister_composer
 from src.main.composer.pet_deleter_composer import pet_deleter_composer
 
+from src.errors.error_handler import handle_errors
+
 pet_route_bp = Blueprint('pets_routes', __name__)
 
 @pet_route_bp.route('/pets', methods=['GET'])
 def list_pets():
-    request = HttpRequest()
-    view = pet_lister_composer()
+    try:
+        request = HttpRequest()
+        view = pet_lister_composer()
 
-    response = view.handle(request)
-
-    return jsonify(response.body), response.status_code
+        response = view.handle(request)
+        return jsonify(response.body), response.status_code
+    except Exception as exception:
+        response = handle_errors(exception)
+        return jsonify(response.body), response.status_code
 
 @pet_route_bp.route('/pets/<string:name>', methods=['DELETE'])
 def delete_pet(name):
-    request = HttpRequest(params={"name": name})
-    view = pet_deleter_composer()
+    try:
+        request = HttpRequest(params={"name": name})
+        view = pet_deleter_composer()
 
-    response = view.handle(request)
-    return jsonify(response.body), response.status_code
+        response = view.handle(request)
+        return jsonify(response.body), response.status_code
+    except Exception as exception:
+        response = handle_errors(exception)
+        return jsonify(response.body), response.status_code
